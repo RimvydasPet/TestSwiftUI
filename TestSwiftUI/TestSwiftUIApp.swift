4f1777a60ae906a6891cad89a05ea9247ae1561d
Rimvydas <rimvydas.petniunas@gmail.com>
2023-10-18 21:39:56 +0300

    Initial Commit

---
+++ TestSwiftUI/TestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftUIApp.swift
+//  TestSwiftUI
+//
+//  Created by Rimvydas on 2023-10-18.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
