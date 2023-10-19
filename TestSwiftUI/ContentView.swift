26f8056eec669b3dbf86b31550e3c46d05aec072
Rimvydas <rimvydas.petniunas@gmail.com>
2023-10-19 18:05:46 +0300

    Starting Project

--- TestSwiftUI/ContentView.swift
+++ TestSwiftUI/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
+        VStack(alignment: .trailing) {
+            Text("Hello, world!")
+            Image( systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
         }
-        .padding()
+        .border(.red)
+        .padding(100)
+        .border(.red)
     }
 }
 
